6ab7142599c96989a416e385f3ffc89cddf8be7f
Darren <xueyublue@gmail.com>
2023-12-09 17:15:41 +0800

    Prepare for docker

--- src/test/java/sg/darren/ms/auth/MsApplicationTests.java
+++ src/test/java/sg/darren/ms/auth/MsApplicationTests.java
@@ -1,9 +1,7 @@
 package sg.darren.ms.auth;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MsApplicationTests {
 
     @Test
